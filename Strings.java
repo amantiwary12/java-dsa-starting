fca73d52bd26155e130f29b13f93e9d26532798c
amantiwary12 <amantiwary2505@gmail.com>
2026-04-06 23:28:15 +0530

    ...

--- Strings.java
+++ Strings.java
@@ -4,32 +4,48 @@
 public class Strings {
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("aman tiWary");
+        StringBuilder sb = new StringBuilder("amantiwary");
 
         // sb.append(" Hello");
         // System.out.println("append : " + sb);
+
+
         // sb.insert(5, "Java ");
         // System.out.println(sb);
+
         // sb.insert(0, "Hello ");
         // System.out.println(sb);
+
         // System.out.println(sb.charAt(5));
         //    
+
         //      sb.append("World"); 
         // sb.replace(6, 10, " Programming ");
         // System.out.println("replace : " + sb);
+
         // sb.delete(0, 7);
         // System.out.println("delete : " + sb);
         //    sb.deleteCharAt(0);
+
         //    sb.reverse();
         //    System.out.println(sb);
+
         //    System.out.println("Length of the string: " + sb.length());
+
         //    System.out.println("Character at index 2: " + sb.charAt(2));
+
         //    System.out.println("Index of 'W': " + sb.indexOf("W"));
+
         //    System.out.println("Last index of 'o': " + sb.lastIndexOf("o"));
+
         //    System.out.println("Substring from index 2 to 5: " + sb.substring(2, 5));
+
         //    System.out.println("String in uppercase: " + sb.toString().toUpperCase());
+
         //    System.out.println("String in lowercase: " + sb.toString().toLowerCase());
+
         // System.out.println("String contains 'Java': " + sb.toString().contains("Java"));
+        
         //    System.out.println("String starts with 'Welcome': " + sb.toString().startsWith("Welcome"));
         //    System.out.println("String ends with 'World': " + sb.toString().endsWith("World"));
         //    System.out.println("Trimmed string: " + sb.toString().trim());
--- oops/inheritance.java
+++ oops/inheritance.java
@@ -1,7 +1,6 @@
 package oops;
 
 //  compole time 
-
 // class calculate {
 // int add( int a , int b){
 //     return a+b;
@@ -9,52 +8,54 @@ package oops;
 // int add( int c , int d , int e){
 //     return c+d+e;
 // }}
-
 // run time 
 class animal {
+
     void sound() {
         System.out.println("animal bark ");
     }
 }
-    class dog extends animal {
-        void sound() {
-            System.out.println("bho bho ");
-        }
+
+class dog extends animal {
+
+    void sound() {
+        System.out.println("bho bho ");
     }
+}
 
-    class cat extends animal {
-        void sound() {
-            System.out.println("meo meo ");
-        }
+class cat extends animal {
+
+    void sound() {
+        System.out.println("meo meo ");
     }
+}
 
-    class cow extends animal {
-        void sound() {
-            System.out.println("mo mo mo  ");
-        }
+class cow extends animal {
+
+    void sound() {
+        System.out.println("mo mo mo  ");
     }
+}
 
-    public class inheritance {
-        public static void main(String[] args) {
-            // { inheritance is a class that acquire the property of another class. it
-            // support code resuability
-            // single inheritance , multiple and herarchial
+public class inheritance {
 
-            // polymorphism allow method to perform different task based on the object that
-            // call them.
-            // run time and compile time }
+    public static void main(String[] args) {
+        // { inheritance is a class that acquire the property of another class. it
+        // support code resuability
+        // single inheritance , multiple and herarchial
 
-            // compile time
-            // calculate calc = new calculate();
-            // System.out.println(calc.add(2,3));
-            // System.out.println(calc.add(3,4,5));
+        // polymorphism allow method to perform different task based on the object that
+        // call them.
+        // run time and compile time }
+        // compile time
+        // calculate calc = new calculate();
+        // System.out.println(calc.add(2,3));
+        // System.out.println(calc.add(3,4,5));
+        // run time
+        animal animal1 = new dog();
+        animal animal2 = new cat();
 
-            // run time
-
-            animal animal1 = new dog();
-            animal animal2 = new cat();
-
-            animal1.sound();
-            animal2.sound();
-        }
+        animal1.sound();
+        animal2.sound();
     }
+}
